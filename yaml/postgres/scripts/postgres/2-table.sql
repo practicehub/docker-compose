67ba669acbcd09c62ff207dfd32ace054bbea72d
Kean <andrew_zhj@msn.com>
2024-11-09 11:10:33 +0800

    up

--- yaml/postgres/scripts/postgres/2-table.sql
+++ yaml/postgres/scripts/postgres/2-table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE postgres.mytable(id integer PRIMARY KEY, purchase_date date, unit integer, description varchar(300));
+CREATE TABLE mytable(id integer PRIMARY KEY, purchase_date date, unit integer, description varchar(300));
